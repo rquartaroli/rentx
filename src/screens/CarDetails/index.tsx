f548c1dd677fe35215284736b5325ab3d9f28627
Rafael Quartaroli <rafaelquartaroli@hotmail.com>
2021-07-20 08:56:05 -0300

    Project finished

---
+++ src/screens/CarDetails/index.tsx
@@ -0,0 +1,172 @@
+import React from 'react';
+import { StatusBar, StyleSheet } from 'react-native';
+import { useTheme } from 'styled-components';
+import { useNavigation, useRoute } from '@react-navigation/native';
+import { getStatusBarHeight } from 'react-native-iphone-x-helper';
+
+import Animated, { 
+  useAnimatedScrollHandler, 
+  useSharedValue,
+  useAnimatedStyle,
+  interpolate,
+  Extrapolate
+} from 'react-native-reanimated';
+
+import { BackButton } from '../../components/BackButton';
+import { ImageSlider } from '../../components/ImageSlider';
+import { Accessory } from '../../components/Accessory';
+import { Button } from '../../components/Button';
+
+import { CarDTO } from '../../dtos/CarDTO';
+import { getAccessoryIcon } from '../../utils/getAccessoryIcon';
+
+import {
+  Container,
+  Header,
+  CarImages,
+  Details,
+  Description,
+  Brand,
+  Name,
+  Rent,
+  Period,
+  Price,
+  About,
+  Accessories,
+  Footer,
+} from './styles';
+
+interface Params {
+  car: CarDTO;
+}
+
+export function CarDetails() {
+  const navigation = useNavigation();
+  const route = useRoute();
+  const { car } = route.params as Params;
+
+  const theme  = useTheme();
+
+  const scrollY = useSharedValue(0);
+  const scrollHandler = useAnimatedScrollHandler(event => {
+    scrollY.value = event.contentOffset.y;
+  });
+
+  const headerStyleAnimation = useAnimatedStyle(() => {
+    return {
+      height: interpolate(
+        scrollY.value,
+        [0, 200],
+        [200, 70],
+        Extrapolate.CLAMP
+      ),
+    }
+  });
+
+  const sliderCarsStyleAnimation = useAnimatedStyle(() => {
+    return {
+      opacity: interpolate(
+        scrollY.value,
+        [0, 150],
+        [1, 0],
+        Extrapolate.CLAMP
+      ),
+    }
+  })
+
+  function handleConfirmRental() {
+    navigation.navigate('Scheduling', { car });
+  }
+
+  function handleBack() {
+    navigation.goBack();
+  }
+
+  return (
+    <Container>
+      <StatusBar 
+        barStyle="dark-content"
+        translucent
+        backgroundColor="transparent"
+      />
+
+      <Animated.View
+        style={[
+          headerStyleAnimation, 
+          styles.header,
+          { backgroundColor: theme.colors.background_secondary }
+        ]}
+      >
+        <Header>
+          <BackButton onPress={handleBack} />
+        </Header>
+
+        <Animated.View style={sliderCarsStyleAnimation}>
+          <CarImages>
+            <ImageSlider 
+              imagesUrl={car.photos} 
+            />
+          </CarImages>
+        </Animated.View>
+      </Animated.View>
+
+      <Animated.ScrollView
+        contentContainerStyle={{
+          paddingHorizontal: 24,
+          paddingTop: getStatusBarHeight() + 160,
+        }}
+        showsVerticalScrollIndicator={false}
+        onScroll={scrollHandler}
+        scrollEventThrottle={16}
+      >
+        <Details>
+          <Description>
+            <Brand>{car.brand}</Brand>
+            <Name>{car.name}</Name>
+          </Description>
+
+          <Rent>
+            <Period>{car.rent.period}</Period>
+            <Price>R$ {car.rent.price}</Price>
+          </Rent>
+        </Details>
+
+        <Accessories>
+          {
+            car.accessories.map(accessory => (
+              <Accessory 
+                key={accessory.type} 
+                name={accessory.name}
+                icon={getAccessoryIcon(accessory.type)} 
+              />
+            ))
+          }
+        </Accessories>
+
+        <About>
+          {car.about}
+          {car.about}
+          {car.about}
+          {car.about}
+          {car.about}
+          {car.about}
+          {car.about}
+          {car.about}
+        </About>
+      </Animated.ScrollView>
+
+      <Footer>
+        <Button title="Escolher período do aluguel" onPress={handleConfirmRental} />
+      </Footer>
+
+    </Container>
+  );
+}
+
+const styles = StyleSheet.create({
+  header: {
+    position: 'absolute',
+    overflow: 'hidden',
+    zIndex: 1,
+  },
+})
---
+++ src/screens/MyCars/index.tsx
@@ -0,0 +1,121 @@
+import React, { useState, useEffect } from 'react';
+import { useNavigation } from '@react-navigation/core';
+import { useTheme } from 'styled-components';
+import { StatusBar, FlatList } from 'react-native';
+
+import api from '../../services/api';
+import { CarDTO } from '../../dtos/CarDTO';
+import { AntDesign } from '@expo/vector-icons';
+
+import { BackButton } from '../../components/BackButton';
+import { LoadAnimation } from '../../components/LoadAnimation';
+import { Car } from '../../components/Car';
+
+import {
+  Container,
+  Header,
+  Title,
+  SubTitle,
+  Content,
+  Appointments,
+  AppointmentsTitle,
+  AppointmentsQuantity,
+  CarWapper,
+  CarFooter,
+  CarFooterTitle,
+  CarFooterPeriod,
+  CarFooterDate,
+} from './styles';
+
+interface CarProps {
+  id: string;
+  user_id: string;
+  car: CarDTO;
+  startDate: string;
+  endDate: string;
+}
+
+export function MyCars() {
+  const [cars, setCars] = useState<CarProps[]>([]);
+  const [loading, setLoading] = useState(true);
+  const navigation = useNavigation();
+  const theme = useTheme();
+
+  function handleBack() {
+    navigation.goBack();
+  }
+
+  useEffect(() => {
+    async function fetchCars() {
+      try{
+        const response = await api.get('/schedules_byuser?user_id=1');
+        setCars(response.data);
+      } catch (error) {
+        console.log(error);
+      } finally{
+        setLoading(false);
+      }
+    }
+
+    fetchCars();
+  }, []);
+
+  return (
+    <Container>
+      <Header>
+        <StatusBar 
+          barStyle="light-content"
+          backgroundColor="transparent"
+          translucent
+        />
+        <BackButton 
+         onPress={handleBack} 
+         color={theme.colors.shape}
+        />
+
+        <Title>
+          Escolha uma {'\n'}
+          data de inicio e {'\n'}
+          fim do aluguel
+        </Title>
+
+        <SubTitle>
+          Conforto, segurança e praticidade.
+        </SubTitle>
+      </Header>
+      { 
+        loading ? <LoadAnimation /> :
+        <Content>
+          <Appointments>
+            <AppointmentsTitle>Agendamentos feitos</AppointmentsTitle>
+            <AppointmentsQuantity>{cars.length}</AppointmentsQuantity>
+          </Appointments>
+
+          <FlatList 
+            data={cars}
+            keyExtractor={item => String(item.id)}
+            showsVerticalScrollIndicator={false}
+            renderItem={({ item }) =>( 
+              <CarWapper>
+                <Car data={item.car} />
+                <CarFooter>
+                  <CarFooterTitle>Peíodo</CarFooterTitle>
+                  <CarFooterPeriod>
+                    <CarFooterDate>{item.startDate}</CarFooterDate>
+                    <AntDesign 
+                      name="arrowright"
+                      size={20}
+                      color={theme.colors.title}
+                      style={{ marginHorizontal: 10 }}
+                    />
+                    <CarFooterDate>{item.endDate}</CarFooterDate>
+                  </CarFooterPeriod>
+                </CarFooter>
+              </CarWapper>
+            )}
+          />
+        </Content>
+      }
+    </Container>
+  );
+}
